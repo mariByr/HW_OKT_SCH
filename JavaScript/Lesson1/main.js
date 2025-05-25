069db5b3f4e36134c6a2da50fe9b0f58e037ff94
mariByr <byramarichka@gmail.com>
2025-05-25 21:53:33 +0300

    add HW_9_3 and HW_9_4

---
+++ JavaScript/Lesson1/main.js
@@ -0,0 +1,37 @@
+ let greeting = "hello";
+console.log(greeting);
+let content = "owu";
+console.log(content);
+let adres = "ua";
+console.log(adres);
+let number = 1;
+console.log(number);
+let number_1 =10;
+console.log(number_1);
+let number_2 = -999;
+console.log(number_2);
+let number_3 = 123;
+console.log(number_3);
+const pi = 3.14;
+console.log(pi);
+let part = 2.7;
+console.log(part);
+let isActive = true;
+let isVisible = false;
+console.log(isActive,isVisible);
+
+// 6Qb97gsv
+
+let firstName = "Maria";
+let middleName = "Stepanivna";
+let lastName = "Byra";
+console.log(firstName + " " + middleName + " " + lastName);
+
+// 4NOy5tufA
+
+ let a = 100;
+ console.log( typeof a);
+ let b = "100";
+ concole.log(typeof b);
+ let c = true;
+ console.log(typeof c);
