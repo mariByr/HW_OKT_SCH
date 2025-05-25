930f41c23f97e51116d50fe8cb10220edbec2e8b
mariByr <byramarichka@gmail.com>
2025-05-25 22:34:09 +0300

    add HW_9_3 and HW_9_4

---
+++ JavaScript/Lesson2/task#67kfznmiMl/main.js
@@ -0,0 +1,11 @@
+let arr = ["hello", "asd", true, false, undefined, [], 567, 0.5, 0.9, NaN];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+console.log(arr[6]);
+console.log(arr[7]);
+console.log(arr[9]);
+console.log(arr[10]);
