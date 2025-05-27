a6c661936298be1d9d9276846a94ed9c572b10a1
mariByr <byramarichka@gmail.com>
2025-05-27 11:29:01 +0300

    add task 1 and 2

--- JavaScript/Lesson2/task_1_2/main.js
+++ JavaScript/Lesson2/task_1_2/main.js
@@ -1,3 +1,4 @@
+// #67kfznmiMl
 let arr = ["hello", "asd", true, false, undefined, [], 567, 0.5, 0.9, NaN];
 console.log(arr[0]);
 console.log(arr[1]);
@@ -9,3 +10,8 @@ console.log(arr[6]);
 console.log(arr[7]);
 console.log(arr[9]);
 console.log(arr[10]);
+// #LARqoUj5I
+
+let book_1 = {title: "A", pageCount: 540, genre: "fiction"};
+let book_2 = {title: "B", pageCount: 720, genre: "fiction"};
+let book_3 = {title: "C", pageCoun : 150, genre: "fiction"};
