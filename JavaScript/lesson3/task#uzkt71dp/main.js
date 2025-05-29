01128d9e75c1d69112df7abdd4550abac479877a
mariByr <byramarichka@gmail.com>
2025-05-30 01:35:07 +0300

    change

--- JavaScript/lesson3/task#uzkt71dp/main.js
+++ JavaScript/lesson3/task#uzkt71dp/main.js
@@ -1,6 +1,10 @@
 
 //– За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом всередині.
 
-for (let i = 0; i < 20; i++) {
+
+
+let i = 0;
+while (i < 20) {
     document.write(`<h1> Hello my friends</h1>`);
+    i++ ;
 }
