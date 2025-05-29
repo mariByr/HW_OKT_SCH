1cd7e77e735e87b0aff42cee6e17c231cf236be8
mariByr <byramarichka@gmail.com>
2025-05-29 17:22:14 +0300

    change

--- JavaScript/lesson3/taskLast/main.js
+++ JavaScript/lesson3/taskLast/main.js
@@ -231,25 +231,25 @@ let usersList = [
     }
 ];
 for (const user of usersList) {
- document.write(`<div class=”users-box”>
-
-            <div class=”user-block”>
-
-                <h2>${user.id} –${user.name} – ${user.username} </h2>
-
-                <h3>${user.email} –${user.phone}</h3>
-
-                <div class=”address-block”>
-
-                    <p>City –${user.address.city}</p>
-
-                    <p>Street –${user.address.street}</p>
-
-                    <p>Suite –${ user.address.suite}</p>
-
-                    <p>Zip code –${ user.address.zipcode}</p>
-
-                </div>
-
-            </div>`)
+ document.write(`<div class="grid-conteiner"><div class=”users-box”>
+ 
+             <div class=”user-block”>
+ 
+                 <h2>${user.id} –${user.name} – ${user.username} </h2>
+ 
+                 <h3>${user.email} –${user.phone}</h3>
+ 
+                 <div class=”address-block”>
+ 
+                     <p>City –${user.address.city}</p>
+ 
+                     <p>Street –${user.address.street}</p>
+ 
+                     <p>Suite –${ user.address.suite}</p>
+ 
+                     <p>Zip code –${ user.address.zipcode}</p>
+ 
+                 </div>
+ 
+             </div></div>`)
 }
