b1f8f71b79414f0b7ac66a7b2e4f337c47960e6b
mariByr <byramarichka@gmail.com>
2025-05-30 20:11:44 +0300

    add tasks

---
+++ JavaScript/add_tasks/hw3-1/main.js
@@ -0,0 +1,39 @@
+
+//– Дано 2 масиви з рівною кількістю об’єктів.
+//
+// Масиви:
+//З’єднати в один об’єкт користувача та місто з відповідними “id” та “user_id”.
+//
+// Записати цей об’єкт в новий масив
+
+let usersWithId = [
+
+    {id: 1, name: 'vasya', age: 31, status: false},
+
+    {id: 2, name: 'petya', age: 30, status: true},
+
+    {id: 3, name: 'kolya', age: 29, status: true},
+
+    {id: 4, name: 'olya', age: 28, status: false}
+
+];
+
+
+
+
+let citiesWithId = [
+
+    {user_id: 3, country: 'USA', city: 'Portland'},
+
+    {user_id: 1, country: 'Ukraine', city: 'Ternopil'},
+
+    {user_id: 2, country: 'Poland', city: 'Krakow'},
+
+    {user_id: 4, country: 'USA', city: 'Miami'}
+
+];
+const combined = []
+for (let i = 0; i < combined.length; i++) {
+    const combinedElement = combined[i];
+
+}
