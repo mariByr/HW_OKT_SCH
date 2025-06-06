1fc69127be798c59aa3349c2d1dc7cf778ab812e
mariByr <byramarichka@gmail.com>
2025-06-06 17:48:51 +0300

    add change

---
+++ JavaScript/add_tasks/add-2/main-2.js
@@ -0,0 +1,13 @@
+
+// Створити цикл for на 100 ітерацій з кроком 2. Вивести поточний номер кроку через console.log та document.write
+
+for (let i = 0; i < 100; i+=2) {
+    console.log(`крок №  ${i}`);
+    document.write(`крок №  ${i}`)
+
+}
+let emptyarr = [ ]
+for (let y = 0; y < 50; y++) {
+        emptyarr.push(y*2);
+            }
+console.log(emptyarr);
---
+++ JavaScript/add_tasks/add-2/main.js
@@ -0,0 +1,15 @@
+
+ // замінити кожне число, кратне 3, на слово “okten”
+ // вивести масив у зворотньому порядку.
+
+ let arr = [2,17,13,6,22,31,45,66,100,-18] ;
+ for (let i = 0; i < arr.length; i++) {
+     if(arr[i] % 3 ===0){
+         console.log(arr[i]);
+
+ }
+ }
+ for (let k = arr.length -1; k >=0; k--) {
+     console.log(arr[k]);s24slNyz7
+
+ }
