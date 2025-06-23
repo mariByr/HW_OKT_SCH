a198d5828ca315ede25dd0095da23331ce87d1f9
mariByr <byramarichka@gmail.com>
2025-06-23 19:18:32 +0300

    add change to first hw

---
+++ JavaScript/Lesson1/#6Qb97gsv/main.js
@@ -0,0 +1,8 @@
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З’єднати їх в одну змінну person (Не об’єкт, просто за допомогою конкатенації)
+
+//
+let firstName = "Maria";
+ let middleName= "Stepanivna";
+let lastName = "Byra";
+console.log( firstName + " " + middleName + " " + lastName );
