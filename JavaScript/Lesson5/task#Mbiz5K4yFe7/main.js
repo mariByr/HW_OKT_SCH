4bb092ec855119a910536072d8f15305df809350
mariByr <byramarichka@gmail.com>
2025-07-04 19:43:26 +0300

    add change to 5 task

---
+++ JavaScript/Lesson5/task#Mbiz5K4yFe7/main.js
@@ -0,0 +1,5 @@
+
+//– створити функцію, яка обчислює та повертає площу циліндру висотою h, та радіусом r,  2*p*r*h
+let cilinderSquare =( radius, height) => {
+    return 2 *Math.PI * radius * height;}
+console.log(cilinderSquare(10,20));
