edc3a0f8faee56d5d6d071bd99a8413c48908af7
mariByr <byramarichka@gmail.com>
2025-07-04 20:03:02 +0300

    add change to 5 task

---
+++ JavaScript/Lesson5/task#SIdMd0hQ/main.js
@@ -0,0 +1,9 @@
+
+//– створити функцію, яка приймає масив та виводить кожен його елемент
+
+let foobar = (array) => {
+    for (const arrElement of array){
+        console.log( arrElement);
+
+    }
+}
