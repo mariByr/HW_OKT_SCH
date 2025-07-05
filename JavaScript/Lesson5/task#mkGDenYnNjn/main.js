abb64e18ad751db81488f36c8e4452b6a1c6555e
mariByr <byramarichka@gmail.com>
2025-07-05 23:41:56 +0300

    add last task

---
+++ JavaScript/Lesson5/task#mkGDenYnNjn/main.js
@@ -0,0 +1,16 @@
+
+//– Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
+//
+// Приклад exchange(10000,[{currency:’USD’,value:40},{currency:’EUR’,value:42}],’USD’) // => 250
+let exchange =(sumUAH,currencyValues,exchangeCurrency) =>{
+    if (exchangeCurrency === 'USD') {
+        resultUsd = sumUAH / currencyValues[0].value;
+        console.log(resultUsd);
+    } else if (exchangeCurrency === 'EUR') {
+        resultEuro= sumUAH / currencyValues[1].value;
+        console.log(resultEuro);
+    } else {
+        console.log('unknown exchange type');
+    }
+}
+exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD');
