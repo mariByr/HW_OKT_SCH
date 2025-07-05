699938ccfbdd09c799918e2c694bb7a73f73d218
mariByr <byramarichka@gmail.com>
2025-07-05 17:49:22 +0300

    add task

--- JavaScript/Lesson5/task#kpsbSQCt2Lf/main.js
+++ JavaScript/Lesson5/task#kpsbSQCt2Lf/main.js
@@ -1,6 +1,11 @@
 
 // створити функцію swap створити функцію swap(arr,index1,index2). Функція міняє місцями значення у відповідних індексах). Функція міняє місцями значення у відповідних індексах
 
-let swap =(arr,index1, index2) =>{
-    if
+let swap =(arr,i1, i2) =>{
+    if(i1 <= arr.length && i2 <= arr.length){
+        let temp = arr[i];
+        arr[i1] = arr[i2];
+        arr[i2] = temp;
+        return arr;
+    }
 }
