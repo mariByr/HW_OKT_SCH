46c3077fdbb193b0a0e6bb9895a7c2616729c400
mariByr <byramarichka@gmail.com>
2025-07-06 18:31:15 +0300

    1,2,4 task

---
+++ JavaScript/Lesson6/task5-1and2/main.js
@@ -0,0 +1,30 @@
+
+//#dFeorS3m7u,#8lld9HMxXWB
+//
+// – Знайти та вивести довжину наступних стрінгових значень
+//– Перевести до великого регістру наступні стрінгові значення
+//  ‘hello world’
+//
+// ‘lorem ipsum’
+//
+// ‘javascript is cool’
+
+let str1 = "hello world!";
+let str2 = "lorem ipsum";
+let str3 =  "javascript is cool";
+console.log(str1.length, str2.length, str3.length);
+let str4 = str1.toUpperCase()
+console.log(str4);
+let str5 = str2.toUpperCase()
+console.log(str5);
+let str6 = str3.toUpperCase()
+console.log(str6);
+// #0b89BkYZwu
+//
+// – Є “брудна” стрінга let str = ‘ dirty string   ‘ . Почистити її від зайвих проб
+
+let str = ' dirty string '
+let s = str.trim()
+console.log(s);
+console.log(s.length);
+console.log(str.length);
