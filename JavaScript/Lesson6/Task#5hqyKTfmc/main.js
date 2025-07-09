a9cf93de8aa16591ce001d17e618f4ffb4482c0f
mariByr <byramarichka@gmail.com>
2025-07-09 13:53:36 +0300

    add task

---
+++ JavaScript/Lesson6/Task#5hqyKTfmc/main.js
@@ -0,0 +1,10 @@
+
+//– створити функцію sortNums(array,direction), яка приймає масив чисел, та сортує його від більшого до меншого, або навпаки  – залежно від значення аргументу direction.
+//
+// let nums = [11,21,3];
+//
+// sortNums(nums,’ascending’) // [3,11,21]
+//
+// sortNums(nums,’descending’) // [21,11,3]
+
+let sortNumbs = numb.sort(numb   => {})
