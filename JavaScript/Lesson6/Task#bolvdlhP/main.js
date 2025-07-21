404fa50cbb447d710def139fe6be806753f67291
mariByr <byramarichka@gmail.com>
2025-07-21 19:09:43 +0300

    add task

--- JavaScript/Lesson6/Task#bolvdlhP/main.js
+++ JavaScript/Lesson6/Task#bolvdlhP/main.js
@@ -14,13 +14,39 @@
  // – всі трефи від 9 та більше
 // Приклад моделі об’єкту карти:
 // {
-//     cardSuit: ”, // ‘spade’, ‘diamond’,’heart’, ‘clubs’
+//     cardSuit: ”, // ‘spade’, ‘diamond’, ’heart’, ‘clubs’
 
-    // value: ”, // ‘6’-’10’, ‘ace’,’jack,’queen’,’king’
+    // value: ,// 6-10, ‘ace’,’jack, ’queen’, ’king’
 
-    // color:”, // ‘red’,’black’
+    // color:, // ‘red,’black’
 
 // }
 
-let cardSuit = ['spade', 'diamond','heart','clubs' ];
-let value = [6, 7, 8, 9, 10, 'ace','jack','queen','king'];
+let cardSuits = ['spade', 'diamond','heart','clubs' ];
+let values = [6, 7, 8, 9, 10, 'ace','jack','queen','king'];
+let deck= []
+
+        for (const cardSuit of cardSuits) {
+            for (const value of values){
+                let color
+                if (cardSuit === 'spade'|| cardSuit
+                    ==='clubs' )
+                    color = 'black';
+                else color = 'red';
+                deck.push({
+                    suit: cardSuit,
+                    value:value,
+                    color: color,
+                    });
+            }
+        }
+  console.log(deck);
+        console.log(deck.find(card=>card.suit==='clubs'&& card.value==='ace'));
+        allSix= deck.filter(card=>card.value === 6);
+        console.log(allSix);
+        allRedCards = deck.filter(card=>card.color === 'red');
+        console.log(allRedCards);
+        allDiamonds = deck.filter(card=>card.suit === 'diamond');
+        console.log(allDiamonds);
+        allClubs = deck.filter(card=>card.suit==='clubs' );
+        console.log(allClubs);
