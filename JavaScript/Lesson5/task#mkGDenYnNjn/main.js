68ec32cdb6baaa7ee9e62af866ac3cd660533206
mariByr <byramarichka@gmail.com>
2025-07-26 19:56:21 +0300

    add change to lesson 7

--- JavaScript/Lesson5/task#mkGDenYnNjn/main.js
+++ JavaScript/Lesson5/task#mkGDenYnNjn/main.js
@@ -2,15 +2,19 @@
 //– Написати функцію обміну валюти exchange(sumUAH,currencyValues,exchangeCurrency)
 //
 // Приклад exchange(10000,[{currency:’USD’,value:40},{currency:’EUR’,value:42}],’USD’) // => 250
-let exchange =(sumUAH,currencyValues,exchangeCurrency) =>{
-    if (exchangeCurrency === 'USD') {
-        resultUsd = sumUAH / currencyValues[0].value;
-        console.log(resultUsd);
-    } else if (exchangeCurrency === 'EUR') {
-        resultEuro= sumUAH / currencyValues[1].value;
-        console.log(resultEuro);
-    } else {
-        console.log('unknown exchange type');
+let exchange =(sumUAH,currencyValues,exchangeCurrency) => {
+    for (const currency of currencyValues) {
+        if (currency.currency === exchangeCurrency) {
+            console.log(
+                sumUAH / currency.value)
+        }
+    else {
+            console.log('Обмін даної валюти не здійснюється')
+        }
     }
+
 }
-exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD');
+
+exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD')
+exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'PLN')
+exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'EUR')
