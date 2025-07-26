353b8e5f110fd6c7bc9b984ecbd670d3fc990ec9
mariByr <byramarichka@gmail.com>
2025-07-26 18:49:09 +0300

    add change to lesson 7

---
+++ JavaScript/add_tasks/add-2/gpttasks/main.js
@@ -0,0 +1,23 @@
+
+//transformation or refactoring or rewritethe function
+function squere(n){
+  return n * n;
+}
+ const sguere2 = (n) => n*n;
+// function sayHello(name){
+//     return 'Hello ,' + name + '!';
+// }
+let sayHello = (name) => {
+    return 'Hello ,' + name + '!';
+}
+// function getArea(width, height) {
+//     return width * height
+// }
+let getArea = (width, height) => width * height;
+// function formatUser(name, age){
+//     let message = 'user'+ name;
+//      message +=',Age:'+ age;
+//     return message;
+// }
+//
+const formatUser = ( name, age) =>`User: ${name} Age: S{age}`;
---
+++ JavaScript/lesson7/tasks#8abtVjRv/main.js
@@ -0,0 +1,17 @@
+
+//#nkMXISv
+//
+// – створити конструктор для об’єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
+//
+// створити пустий масив, наповнити його 10 об’єктами Client
+function Client(id, name, surname , email, phone, order){
+    this.id = id;
+    this.name = name;
+    this.surname = surname;
+    this.email = email;
+    this.phone = phone;
+    this.order = order;
+}
+function Product(title, price){
+
+}
