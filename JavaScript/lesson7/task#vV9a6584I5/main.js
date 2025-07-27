e86f900a4df0cb1b4ec13f6b33df89ec98780ffe
mariByr <byramarichka@gmail.com>
2025-07-28 00:05:30 +0300

    add change

--- JavaScript/lesson7/task#vV9a6584I5/main.js
+++ JavaScript/lesson7/task#vV9a6584I5/main.js
@@ -10,3 +10,36 @@
 //     — changeYear (newValue) – змінює рік випуску на значення newValue
 //
 //     — addDriver (driver) – приймає об’єкт який “водій” з довільним набором полів, і додає його в поточний об’єкт car
+
+function Car(model, produser, year,maxSpeed, volumeEngine){
+    this.model = model;
+    this.produser = produser;
+    this.year = year;
+    this.maxSpeed = maxSpeed;
+    this.volumeEngine = volumeEngine;
+    this.drive = function(){console.log(`їдемо зі швидкістю ${this.maxSpeed} на годину`)}
+    this.info = function(){
+        for (const key of this) {
+            console.log(key, this[key]);
+        }
+    this.increaseMaxSpeed = function (newSpeed) {
+        this.maxSpeed =  this.maxSpeed+newSpeed};
+    this. changeYear = function (year){
+     if(year>1815) this.year = year
+    };
+    this.addDriver = function (driver) {
+        if (driver) this.driver = driver;
+
+    }
+        }
+    let car= new Car('Skoda', 'Skoda Auto', 1998, 170,  1,4 );
+    console.log(car);
+    car.drive();
+    car.info();
+    car.increaseMaxSpeed(100)
+    car.changeYear(2015)
+    car.addDriver({})
+    console.log(car)
+
+
+}
