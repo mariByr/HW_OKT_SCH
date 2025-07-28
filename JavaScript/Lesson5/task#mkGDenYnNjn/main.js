bce514a7f84b21c6bdab832c97c412d65868a015
mariByr <byramarichka@gmail.com>
2025-07-29 00:03:20 +0300

    add change

--- JavaScript/Lesson5/task#mkGDenYnNjn/main.js
+++ JavaScript/Lesson5/task#mkGDenYnNjn/main.js
@@ -3,18 +3,21 @@
 //
 // Приклад exchange(10000,[{currency:’USD’,value:40},{currency:’EUR’,value:42}],’USD’) // => 250
 let exchange =(sumUAH,currencyValues,exchangeCurrency) => {
+    let found = false;
     for (const currency of currencyValues) {
         if (currency.currency === exchangeCurrency) {
-            console.log(
-                sumUAH / currency.value)
-        }
-    else {
-            console.log('Обмін даної валюти не здійснюється')
+            console.log(sumUAH / currency.value)
+            found = true;
         }
     }
-
+    if (!found) {
+        console.log('Обмін даної валюти не здійснюється')
+    }
 }
+
+
+
 
 exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'USD')
 exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'PLN')
-exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'EUR')
+// exchange(10000,[{currency:'USD',value:25},{currency:'EUR',value:42}],'EUR')
