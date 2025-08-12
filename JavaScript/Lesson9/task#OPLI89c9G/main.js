96e6fb9cefc971d519740f756d3acdbcd73d081b
mariByr <byramarichka@gmail.com>
2025-08-12 20:23:43 +0300

    add change

--- JavaScript/Lesson9/task#OPLI89c9G/main.js
+++ JavaScript/Lesson9/task#OPLI89c9G/main.js
@@ -6,4 +6,10 @@
 // Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
 
 const ul = document.createElement('ul')
-arr= ['Main','Products','About us','Contactcts',];
+arrInfo = ['Main','Products','About us','Contactcts'];
+for (const  item of arrInfo) {
+    const li = document.createElement('li');
+li.innerText = item;
+ul.appendChild(li);
+}
+document.body.appendChild(ul);
