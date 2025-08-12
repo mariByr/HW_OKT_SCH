5c15b6dbfcaa7e2f0fd7c07c6f3d28ca91ba7c93
mariByr <byramarichka@gmail.com>
2025-08-12 18:43:04 +0300

    add change

---
+++ JavaScript/Lesson9/task#OPLI89c9G/main.js
@@ -0,0 +1,9 @@
+
+//– Є масив:
+//
+// [‘Main’,’Products’,’About us’,’Contacts’]
+//
+// Зробити ul в середині якої будуть лежати елементи масиву (кожен в своєму li)
+
+const ul = document.createElement('ul')
+arr= ['Main','Products','About us','Contactcts',];
