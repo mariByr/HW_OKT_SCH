4bcf35b7df85ba34da791055b8974ae755ad3494
mariByr <byramarichka@gmail.com>
2025-09-11 22:36:47 +0300

    add change

--- JavaScript/Lesson10/task#NKB0tgWIK1G-last/main.js
+++ JavaScript/Lesson10/task#NKB0tgWIK1G-last/main.js
@@ -7,15 +7,15 @@
 //
 // При натисканні prev виводяться попередні 10 об’єктів
 
-const items= Array.from({length:100}, (_,i) => `Item ${i+1} `);
-const pageSize= 10;
-let currentPage=0;
-function render () {
+  const items= Array.from({length:100}, (_,i) => `Item ${i+1} `); //cтворення масиву
+const pageSize= 10;  //   змінна яка показує скільки обєктів буде виводитись при рендиренгу
+let currentPage=0;   //початкова сторінка
+function render () {      //частина коду яка буде виконувватись в при будь-якому натиску в вигляді функціїфункція яка буде виконуватись при натисканні всюди однакова
     const list = document.getElementById('list');
     list.innerText= '';
-    const start = currentPage * pageSize;
-    const end = start + pageSize;
-    const pageItems =items.slice(start, end);
+    const start = currentPage * pageSize;//показує з якого індексу буде вирізати
+    const end = start + pageSize;//по який не включно
+    const pageItems =items.slice(start, end);//за допомогою слайсу робимо масив який будем виводити потім при натиску кнопки
     pageItems.forEach((item) => {let div = document.createElement('div');
     div.classList.add('item');
     div.innerText = item;
