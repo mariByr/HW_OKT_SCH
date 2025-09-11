6a49365894be9549a226194fe4a14826f1eb2d6a
mariByr <byramarichka@gmail.com>
2025-09-11 21:51:43 +0300

    add change

--- JavaScript/Lesson10/task#NKB0tgWIK1G-last/main.js
+++ JavaScript/Lesson10/task#NKB0tgWIK1G-last/main.js
@@ -7,4 +7,18 @@
 //
 // При натисканні prev виводяться попередні 10 об’єктів
 
-const objects=
+const items= Array.from({length:100}, (_,i) => `Item ${i+1} `);
+const pageSize= 10;
+const currentPage=0;
+function render () {
+    const list = document.getElementById('list');
+    list.innerText= '';
+    const start = currentPage * pageSize;
+    const end = start + pageSize;
+    const pageItems =items.slice(start, end);
+    pageItems.forEach((item) => {let div = document.createElement('div');
+    div.classList.add('item');
+    div.innerText = item;
+    list.appendChild(div);
+    })
+}
---
+++ JavaScript/Lesson10/task#bq1zkx7WP/mai8n.js
@@ -0,0 +1,16 @@
+
+//*** (подібне було вище, але…будьте уважні в другій частині) створити сторінку з довільним блоком, в середині якого є значення “100грн”
+//
+// при перезавантаженні сторінки до значення додається по 10грн, але !!! зміна ціни відбувається тільки на перезавантаження, які відбулись пізніше ніж 10 секунд після попереднього.
+//
+//  При перезавантаженні, яке відбулось раніше ніж минуло 10 секунд, нічого не відбувається
+let now =  new Date().getTime();
+let price = +localStorage.getItem("priceKey") || 100;
+const last = +localStorage.getItem("timeKey") || 0;
+const tenSecond = 10_000;
+if (now -last>=tenSecond) {
+    price += 10;
+
+    localStorage.setItem('priceKey', JSON.stringify(price));
+    localStorage.setItem('timeKey', JSON.stringify(now));}
+    document.getElementById("price").innerText = price;
