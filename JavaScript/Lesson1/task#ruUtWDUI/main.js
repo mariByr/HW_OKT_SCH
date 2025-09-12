7e0d0b9680da8891c2d916e32812c7f851a94e0f
mariByr <byramarichka@gmail.com>
2025-09-12 21:23:03 +0300

    add change

---
+++ JavaScript/Lesson1/task#ruUtWDUI/main.js
@@ -0,0 +1,9 @@
+
+//Додаткове для тих, хто цікавився promptoм,отримання даних від користувача і подальше використання
+
+// За допомогою 3-х різних prompt() отримати 3 слова які являються вашими Імʼям, По-батькові та роками. Та вивести в консоль
+
+let name = prompt('Please, enter your name');
+let age = prompt('Please, enter your age');
+let surname = prompt('Please, enter your surname');
+console.log(name, age, surname);
