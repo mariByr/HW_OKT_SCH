09f6f0d0eb860bcbc9caa522ad9f217fc15b334a
mariByr <byramarichka@gmail.com>
2025-10-26 00:00:19 +0300

    add change

--- JavaScript/Lesson11/task#whXxOBlYS0H/main11.js
+++ JavaScript/Lesson11/task#whXxOBlYS0H/main11.js
@@ -1,4 +1,21 @@
 
 //– взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини
+ const cartsDiv = document.querySelector('#carts')
 fetch('https://dummyjson.com/carts')
-    .then(res => res.json())
+    .then(value => value.json())
+    .then(cartsObject => {
+        const {carts} = cartsObject;
+        console.log(carts);
+        for (const cart of carts) {
+            const div = document.createElement('div');
+            div.classList.add('cart-container');
+            div.innerText = `${cart.id} ${cart.total} ${cart.discountedTotal}`
+cartsDiv.append(div);
+        }
+//"total": 103774.85,
+//       "discountedTotal": 89686.65,
+//       "userId": 33,
+//       "totalProducts": 4,
+//       "totalQuantity": 15
+//     },
+        })
