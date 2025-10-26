d2009e7ee2b7bff20dfdcdc00f71daef2bc15fb6
mariByr <byramarichka@gmail.com>
2025-10-26 22:35:09 +0200

    add task1

--- JavaScript/Lesson11/task#whXxOBlYS0H/main11.js
+++ JavaScript/Lesson11/task#whXxOBlYS0H/main11.js
@@ -5,17 +5,34 @@ fetch('https://dummyjson.com/carts')
     .then(value => value.json())
     .then(cartsObject => {
         const {carts} = cartsObject;
-        console.log(carts);
-        for (const cart of carts) {
+               for (const cart of carts) {
             const div = document.createElement('div');
             div.classList.add('cart-container');
-            div.innerText = `${cart.id} ${cart.total} ${cart.discountedTotal}`
+         const divWithInfo = document.createElement('div');
+         divWithInfo.innerText =`
+             "total": ${cart.total},
+      "discountedTotal": ${cart.discountedTotal},
+      "userId":${cart.userId},
+      "totalProducts": ${cart.totalProducts},
+      "totalQuantity": ${cart.totalQuantity};`
+            const ol=document.createElement("ol");
+            for (const product of cart.products) {
+                const li = document.createElement("li");
+                const info = document.createElement("p");
+                info.innerText =`${product.id},
+          "title":${product.title}, 
+          "quantity":${product.quantity}, ,
+          "total":${product.total},
+          "discountPercentage":${product.discountPercentage}, ,
+          "discountedTotal":${product.discountedTotal}, 
+          `
+                const img = document.createElement("img");
+                img.src=product.thumbnail;
+                li.append(img,info)
+                 ol.appendChild( li)
+            }
+div.append(divWithInfo,ol);
 cartsDiv.append(div);
         }
-//"total": 103774.85,
-//       "discountedTotal": 89686.65,
-//       "userId": 33,
-//       "totalProducts": 4,
-//       "totalQuantity": 15
-//     },
+
         })
