6c1818bb4564a1ba6eab078631550ada91957e3b
mariByr <byramarichka@gmail.com>
2025-10-26 23:40:56 +0200

    add task11/2

--- JavaScript/Lesson11/task#whXxOBlYS0H/main.js
+++ JavaScript/Lesson11/task#whXxOBlYS0H/main.js
@@ -1,2 +1,13 @@
 
-// – взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути списком під час відображення.
+// – взяти https://dummyjson.com/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути списком під час відображення.
+fetch('https://dummyjson.com/recipes')
+    .then(value => value.json())
+.then(recipesObject => {
+    const {recipes} = recipesObject;
+    for (const recipe of recipes) {
+        const resipeDiv = document.createElement('div');
+        resipeDiv.classList.add('recipe');
+        recipe.innerText = `${recipe.name} - ${recipe.description}`;
+    }
+    }
+)
