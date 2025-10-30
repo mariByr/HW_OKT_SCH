ad9b97d956ba38886c6be6d1ec7bfc8fd6c5e042
mariByr <byramarichka@gmail.com>
2025-10-30 23:19:44 +0200

    add changes

--- JavaScript/Lesson11/task#whXxOBlYS0H/main.js
+++ JavaScript/Lesson11/task#whXxOBlYS0H/main.js
@@ -9,22 +9,33 @@ fetch('https://dummyjson.com/recipes')
         const recipDiv = document.createElement('div');
         recipDiv.classList.add('recipe');
         const divIdName=document.createElement('div');
-        divIdName.innerText = `"id":${recipe.id}, 
-        "name":${recipe.name}`;
+        divIdName.classList.add('recipe-name');
+        divIdName.innerHTML = `"Id":${recipe.id}, 
+        "Name":${recipe.name}`;
+        const divInstruction = document.createElement('div');
+        const h3inst = document.createElement('h3');
+        h3inst.innerText= `Instructions:`;
+        const instructionsOl = document.createElement('ol');
+        const steps = Array.isArray(recipe.instructions)?
+            recipe.instructions : [];
+        for (const instructionElement of steps) {
+            const li=document.createElement('li');
+            li.innerText=instructionElement;
+          instructionsOl.appendChild(li)
+            divInstruction.append(h3inst, instructionsOl)
+        }
         const infoDiv = document.createElement('div');
         infoDiv.innerText = `
-         "instruction": ${recipe.instruction},
-        "cookTimeMinutes": ${recipe.cookTimeMinutes},
-        "servings":  ${recipe.servings},
-        "difficulty": ${recipe.difficulty},
-        "cuisine": ${recipe.cuisine},
-        "caloriesPerServing": ${recipe.caloriesPerServing}`
-        // "tags": ${recipe.tags}`
+        "CookTimeMinutes": ${recipe.cookTimeMinutes},
+        "Servings":  ${recipe.servings},
+        "Difficulty": ${recipe.difficulty},
+        "Cuisine": ${recipe.cuisine},
+        "CaloriesPerServing": ${recipe.caloriesPerServing}`
         const ingredientDiv = document.createElement('div');
         const olIngredients= document.createElement('ol');
         olIngredients.classList.add('olIngredients');
        const h3ing = document.createElement('h3');
-       h3ing.innerText= `ingredients:`;
+       h3ing.innerText= `Ingredients:`;
         for (const ingredient of recipe.ingredients) {
             const li = document.createElement('li');
             li.innerText = ingredient;
@@ -32,7 +43,7 @@ fetch('https://dummyjson.com/recipes')
         ingredientDiv.append(h3ing,olIngredients)
         olIngredients.appendChild(li,)
         }
-        recipDiv.append(divIdName,ingredientDiv,infoDiv);
+        recipDiv.append(divIdName,ingredientDiv,divInstruction,infoDiv);
         divWrapper.appendChild(recipDiv);    }
     }
 )
